85cf5400a9cc7eecaa2459c43b06b0434be730f6
unknown <pakphil7@gmail.com>
2020-03-08 18:33:46 -0700

    added first page to Code Quiz, made first page hide when Start Quiz is clicked

---
+++ script.js
@@ -0,0 +1,8 @@
+var startButton = $("#start-button")
+console.log(start)
+var start = document.getElementById("start-button")
+start.addEventListener("click",function(){
+    console.log("click");
+    $("#start-page").hide();
+
+})
